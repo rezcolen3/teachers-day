9aad11ab61b364a5565185b4154209b16af8b4c1
RezHackXYZ <rhythmupa@gmail.com>
2025-09-04 04:34:35 +0530

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../styles/app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<div class="w-screen h-screen">
+	{@render children?.()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="circle">
+		<div class="mail-box">
+			<div class="flag"></div>
+		</div>
+	</div>
+</div>
