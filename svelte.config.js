342b5223a78c7fb49f2aa1571e4101cc5aed5a47
RezHackXYZ <rhythmupa@gmail.com>
2025-09-04 07:17:33 +0530

    saddasasd

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
-import vercel from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite/preprocess.js';
+import adapter from '@sveltejs/adapter-auto';
 
-export default {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: vercel()
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
+
+export default config;
