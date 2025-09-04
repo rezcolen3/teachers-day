3026e082371c8889f48c53a36c734a85f1e86a65
RezHackXYZ <rhythmupa@gmail.com>
2025-09-04 06:09:46 +0530

    eee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,33 @@
+<script>
+	let stage = 1;
+
+	function stage2() {
+		stage++;
+
+		document.querySelector('.in-box').classList.replace('in-box', 'free');
+		document.querySelector('.mail-box').classList.replace('mail-box', 'outside');
+	}
+</script>
+
 <div class="container">
 	<div class="circle">
-		<div class="mail-box">
+		<div class="mail-box" onclick={() => stage2()}>
 			<div class="box-flap"></div>
 			<div class="box-inside"></div>
 			<div class="box-pole"></div>
+			<div class="mail in-box">
+				<div class="mail-content">
+					<span class="from"> <span class="toorfrom"> From: </span> The Boys of Class 7th A </span>
+
+					<span class="to">
+						<span class="toorfrom"> To: </span> The best english teacher, Vani Mam
+					</span>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
 
-
-<div class="mail"></div>
+{#if stage === 2}
+	<div class="letter"></div>
+{/if}
